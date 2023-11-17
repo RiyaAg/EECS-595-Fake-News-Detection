58fb2720d3dbc97c34fd7a573d62f20ce83d018d
Yara El-Tawil <yeltawil@umich.edu>
2023-11-17 00:13:46 -0500

    naive bayes 64 percent

--- naive_bayes.py
+++ naive_bayes.py
@@ -20,6 +20,9 @@ from nltk import word_tokenize
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics import accuracy_score
 from nltk.stem import WordNetLemmatizer
+import numpy as np
+from sklearn.model_selection import GridSearchCV
+
 
 
 
@@ -95,24 +98,49 @@ def train(train_data, train_labels):
         output_string (string):
         An object representing the trained model.
 
-    """
-    trained_model = Classifier()
-    # initialize classifier class
-    #load and preprocess training data
-    # training_data, training_labels = trained_model.load(training_path)
-    print("loaded training data")
-    # print("length:", len(training_data))
+    # """
+    # trained_model = Classifier()
+    # # initialize classifier class
+    # #load and preprocess training data
+    # # training_data, training_labels = trained_model.load(training_path)
+    # print("loaded training data")
+    # # print("length:", len(training_data))
 
 
-    preprocessed_training = trained_model.preprocess(train_data)
+    # preprocessed_training = trained_model.preprocess(train_data)
 
-    #bag of words unigram extraction and fir classifier to training data
-    train = trained_model.count_vectorizer.fit_transform(preprocessed_training)
-    trained_model.classifier.fit(train, train_labels)
+    # #bag of words unigram extraction and fir classifier to training data
+    # train = trained_model.count_vectorizer.fit_transform(preprocessed_training)
+    # trained_model.classifier.fit(train, train_labels)
 
-    # = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = #
+    # # = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = #
+    # return trained_model
+        # Initialize the classifier
+    classifier = Classifier()
 
-    return trained_model
+    # Preprocess training data
+    preprocessed_training = classifier.preprocess(train_data)
+
+    # Perform grid search for hyperparameter tuning
+    param_grid = {'alpha': np.linspace(0.1, 2.0, 10)}  # Example alpha values
+    grid_search = GridSearchCV(MultinomialNB(), param_grid, cv=5, scoring='accuracy')
+    
+    # Fit the vectorizer on the training data
+    X_train = classifier.count_vectorizer.fit_transform(preprocessed_training)
+    
+    grid_search.fit(X_train, train_labels)
+
+    # Get the best hyperparameter value
+    best_alpha = grid_search.best_params_['alpha']
+
+    # Update the classifier with the best hyperparameter
+    classifier.classifier = MultinomialNB(alpha=best_alpha)
+
+    # Fit the classifier to the training data
+    classifier.classifier.fit(X_train, train_labels)
+
+    return classifier
+
 
 
 def predict(trained_model, test_data, test_label):
@@ -164,7 +192,90 @@ def evaluate(model_predictions, ground_truth):
     #use the ground truths to check the accuracy
     accuracy = accuracy_score(ground_truth, model_predictions)
 
-
+    return accuracy
     # = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = #
 
-    return accuracy
+#     return accuracy
+# import re
+# import nltk
+# import numpy as np
+# from sklearn.naive_bayes import MultinomialNB
+# from sklearn.feature_extraction.text import CountVectorizer
+# from sklearn.model_selection import GridSearchCV
+# from sklearn.metrics import accuracy_score
+# from nltk.corpus import stopwords
+# from nltk.stem import WordNetLemmatizer
+# from nltk import word_tokenize
+
+# nltk.download('stopwords')
+# nltk.download('wordnet')
+# nltk.download('punkt')
+
+# class Classifier(object):
+
+#     def __init__(self):
+#         # setup, define classes folder names and initialize vectorizer and classifier
+#         self.count_vectorizer = CountVectorizer(ngram_range=(1, 5))
+#         self.classifier = MultinomialNB()
+
+#     def preprocess(self, data):
+#         processed = []
+#         lemmatizer = WordNetLemmatizer()
+
+#         for review in data:
+#             review = re.sub(r'[^\w\s\d]', '', review.lower())  # Combine lowercase and special character removal
+#             tokens = word_tokenize(review)
+#             stopword_dict = set(stopwords.words('english'))
+#             tokens = [lemmatizer.lemmatize(word) for word in tokens if word not in stopword_dict]
+#             review = ' '.join(tokens)
+#             processed.append(review)
+
+#         return processed
+
+#     def predict(self, test_data):
+#         testing_data = self.preprocess(test_data)
+#         test = self.count_vectorizer.transform(testing_data)
+#         model_predictions = self.classifier.predict(test)
+#         return model_predictions
+
+# def tune_and_train(train_data, train_labels):
+#     # Initialize the classifier
+#     classifier = Classifier()
+
+#     # Preprocess training data
+#     preprocessed_training = classifier.preprocess(train_data)
+
+#     # Perform grid search for hyperparameter tuning
+#     param_grid = {'alpha': np.linspace(0.1, 2.0, 10)}  # Example alpha values
+#     grid_search = GridSearchCV(MultinomialNB(), param_grid, cv=5, scoring='accuracy')
+    
+#     # Fit the vectorizer on the training data
+#     X_train = classifier.count_vectorizer.fit_transform(preprocessed_training)
+    
+#     grid_search.fit(X_train, train_labels)
+
+#     # Get the best hyperparameter value
+#     best_alpha = grid_search.best_params_['alpha']
+
+#     # Update the classifier with the best hyperparameter
+#     classifier.classifier = MultinomialNB(alpha=best_alpha)
+
+#     # Fit the classifier to the training data
+#     classifier.classifier.fit(X_train, train_labels)
+
+#     return classifier
+
+# def evaluate(trained_model, test_data, test_labels):
+#     # Preprocess testing data
+#     testing_data = trained_model.preprocess(test_data)
+
+#     # Transform the testing data using the fitted vectorizer
+#     X_test = trained_model.count_vectorizer.transform(testing_data)
+
+#     # Predict using the trained model
+#     model_predictions = trained_model.predict(testing_data)
+
+#     # Evaluate accuracy
+#     accuracy = accuracy_score(test_labels, model_predictions)
+
+#     return accuracy
