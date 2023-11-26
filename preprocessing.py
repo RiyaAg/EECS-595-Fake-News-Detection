cf0c6709820f6697887b2d144364a2ee3ef4b48a
sawanpatel <sawanp.813@gmail.com>
2023-11-25 23:26:29 -0500

    64% accuracy score w/ binarized SVM

--- preprocessing.py
+++ preprocessing.py
@@ -4,3 +4,10 @@ def set_idx(example, idx):
     """Replaces all instance labels with binary labels given the intended label as input."""
     example['label'] = (example['label'] == idx)
     return example
+
+def binarize(example):
+    """Binarize LIAR Dataset."""
+    label_mapping = {0: 0, 1: 1, 2: 1, 3: 1, 4: 0, 5:1}
+    val = example['label']
+    example['label'] = label_mapping[val]
+    return example
